44cd918fe2d831d628a5e7772b98fc3d7030ee6a
maurizio-dominici <mauriziodominici4@gmail.com>
2025-06-17 14:59:39 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// IMPORTS
+import reactLogo from "./assets/img/react.svg";
+import viteLogo from "/vite.svg";
+import Header from "./layout/Header";
+import Main from "./layout/Main";
+import Footer from "./layout/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+// IMPORTS
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./assets/css/index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
