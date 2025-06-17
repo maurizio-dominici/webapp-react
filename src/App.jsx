bea59dcfe316bba2267b0f14ea9e292a8a364129
maurizio-dominici <mauriziodominici4@gmail.com>
2025-06-17 15:11:41 +0200

    add milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 // IMPORTS
-import reactLogo from "./assets/img/react.svg";
-import viteLogo from "/vite.svg";
-import Header from "./layout/Header";
-import Main from "./layout/Main";
-import Footer from "./layout/Footer";
 
 export default function App() {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <div className="container">
+        <h1>web app react</h1>
+      </div>
     </>
   );
 }
